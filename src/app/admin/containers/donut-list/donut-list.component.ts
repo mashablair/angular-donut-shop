733fbc97a892b902dce25ff048bd63c5cff4f2ed
Masha Blair <mashablair@gmail.com>
2023-08-01 15:04:43 -0400

    Add Donut model to avoid using  as a type for donut and donuts array.

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'donut-list',
@@ -6,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class DonutListComponent implements OnInit {
-  donut!: any;
-  donuts!: any[];
-
-  constructor() {}
+  donut!: Donut;
+  donuts!: Donut[];
 
   ngOnInit(): void {
     this.donuts = [
