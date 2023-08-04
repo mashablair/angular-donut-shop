f6dd6c984f692af12b5c54528d1430f2c5362468
Masha Blair <mashablair@gmail.com>
2023-08-04 16:50:04 -0400

    Added form validation using Angular error messages depending on requirement. Angular can use html attributes.

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -6,12 +6,28 @@ import { Component, OnInit } from '@angular/core';
     <form class="donut-form" #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" ngModel />
+        <input
+          type="text"
+          name="name"
+          class="input"
+          required
+          minlength="5"
+          ngModel
+          #name="ngModel"
+        />
+        <ng-container *ngIf="name.invalid && name.touched">
+          <div class="donut-form-error" *ngIf="name.errors?.minlength">
+            Minimum length of a name is 5!
+          </div>
+          <div class="donut-form-error" *ngIf="name.errors?.required">
+            Name is required.
+          </div>
+        </ng-container>
       </label>
 
-      <label for="">
+      <label>
         <span>Icon</span>
-        <select name="icon" class="input input--select" ngModel>
+        <select name="icon" class="input input--select" required ngModel>
           <option *ngFor="let icon of icons" [ngValue]="icon">
             {{ icon }}
           </option>
@@ -20,13 +36,19 @@ import { Component, OnInit } from '@angular/core';
 
       <label>
         <span>Price</span>
-        <input type="number" name="price" class="input" ngModel />
+        <input type="number" name="price" class="input" required ngModel />
       </label>
 
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" [value]="undefined" ngModel />
+          <input
+            type="radio"
+            name="promo"
+            required
+            [value]="undefined"
+            ngModel
+          />
           <span>None</span>
         </label>
         <label>
@@ -38,7 +60,17 @@ import { Component, OnInit } from '@angular/core';
           <span>Limited</span>
         </label>
       </div>
-      <pre>{{ form.value | json }}</pre>
+
+      <label for="">
+        <span>Description</span>
+        <textarea
+          name="description"
+          class="input input--textarea"
+          required
+          ngModel
+        ></textarea>
+      </label>
+      <pre>{{ form.form.status | json }}</pre>
     </form>
   `,
   styles: [
