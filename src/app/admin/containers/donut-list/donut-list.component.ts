319592349455e6b7cabac82fff5bc36124b56318
Masha Blair <mashablair@gmail.com>
2023-08-04 09:40:35 -0400

    Playing with NgContainer, NgTemplate and flow logic syntax.

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -5,9 +5,21 @@ import { Donut } from '../../models/donut.model';
   selector: 'donut-list',
   template: `
     <div>
-      <donut-card [donut]="donuts[0]"></donut-card>
-      <donut-card [donut]="donuts[1]"></donut-card>
-      <donut-card [donut]="donuts[2]"></donut-card>
+      <ng-container *ngIf="donuts.length">
+        <donut-card [donut]="donuts[0]"></donut-card>
+        <donut-card [donut]="donuts[1]"></donut-card>
+        <donut-card [donut]="donuts[2]"></donut-card>
+      </ng-container>
+
+      <ng-template [ngIf]="donuts.length" [ngIfElse]="nothing" ]>
+        <donut-card [donut]="donuts[0]"></donut-card>
+        <donut-card [donut]="donuts[1]"></donut-card>
+        <donut-card [donut]="donuts[2]"></donut-card>
+      </ng-template>
+
+      <ng-template #nothing>
+        <p>No Donuts here...</p>
+      </ng-template>
     </div>
   `,
   styles: [],
@@ -16,29 +28,6 @@ export class DonutListComponent implements OnInit {
   donuts!: Donut[];
 
   ngOnInit(): void {
-    this.donuts = [
-      {
-        id: 'y8z0As',
-        name: 'Just Chocolate',
-        icon: 'just-chocolate',
-        price: 119,
-        description: 'For the pure chocoholic.',
-      },
-      {
-        id: '3u98Kl',
-        name: 'Glazed Fudge',
-        icon: 'glazed-fudge',
-        price: 129,
-        description: 'Sticky perfection.',
-        promo: true,
-      },
-      {
-        id: 'ae098s',
-        name: 'Caramel Swirl',
-        icon: 'caramel-swirl',
-        price: 129,
-        description: 'Chocolate drizzled with caramel.',
-      },
-    ];
+    this.donuts = [];
   }
 }
