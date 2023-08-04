9b1aa196d0d4e25880fcca0170ad101a3cbf8c86
Masha Blair <mashablair@gmail.com>
2023-08-04 12:53:10 -0400

    Angular rendering flow, displaying donuts with different variations of NgFor loop.

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -5,17 +5,19 @@ import { Donut } from '../../models/donut.model';
   selector: 'donut-list',
   template: `
     <div>
-      <ng-container *ngIf="donuts.length">
-        <donut-card [donut]="donuts[0]"></donut-card>
-        <donut-card [donut]="donuts[1]"></donut-card>
-        <donut-card [donut]="donuts[2]"></donut-card>
+      <ng-container *ngIf="donuts.length; else nothing">
+        <donut-card
+          *ngFor="let donut of donuts; trackBy: trackById"
+          [donut]="donut"
+        ></donut-card>
       </ng-container>
 
-      <ng-template [ngIf]="donuts.length" [ngIfElse]="nothing" ]>
-        <donut-card [donut]="donuts[0]"></donut-card>
-        <donut-card [donut]="donuts[1]"></donut-card>
-        <donut-card [donut]="donuts[2]"></donut-card>
-      </ng-template>
+      <div
+        *ngFor="let item of donuts; index as i; odd as o; even as e"
+        [style.color]="o ? 'red' : 'blue'"
+      >
+        {{ i + 1 }}
+      </div>
 
       <ng-template #nothing>
         <p>No Donuts here...</p>
@@ -28,6 +30,33 @@ export class DonutListComponent implements OnInit {
   donuts!: Donut[];
 
   ngOnInit(): void {
-    this.donuts = [];
+    this.donuts = [
+      {
+        id: 'y8z0As',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic.',
+      },
+      {
+        id: '3u98Kl',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Sticky perfection.',
+        promo: true,
+      },
+      {
+        id: 'ae098s',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel.',
+      },
+    ];
+  }
+
+  trackById(index: number, value: Donut) {
+    return value.id;
   }
 }
