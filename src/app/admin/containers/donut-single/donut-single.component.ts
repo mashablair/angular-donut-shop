79fc04eca11eeac45edbe53b5a0524a8569652d6
Masha Blair <mashablair@gmail.com>
2023-08-08 12:31:47 -0400

    Add one-way data binding to NgModel for all form fields to create immutable data for form editing.  Added service for donut.

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -5,15 +5,26 @@ import { Donut } from '../../models/donut.model';
   selector: 'donut-single',
   template: `
     <div>
-      <donut-form (create)="onCreate($event)"></donut-form>
+      <donut-form [donut]="donut" (create)="onCreate($event)"></donut-form>
     </div>
   `,
   styles: [],
 })
 export class DonutSingleComponent implements OnInit {
+  donut!: Donut;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.donut = {
+      id: 'y8z0As',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      description: 'For the pure chocoholic.',
+      promo: 'limited',
+    };
+  }
 
   onCreate(donut: Donut) {
     console.log('onCreate', donut);
