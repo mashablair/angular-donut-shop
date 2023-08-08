372b3f0eb615e76187a94f0c52be01cacc2fdefd
Masha Blair <mashablair@gmail.com>
2023-08-08 11:55:59 -0400

    Emit Form Payload to Smart Component via @Output

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'donut-form',
@@ -141,6 +142,8 @@ import { NgForm } from '@angular/forms';
   ],
 })
 export class DonutFormComponent {
+  @Output() create = new EventEmitter<Donut>();
+
   icons: string[] = [
     'caramel-swirl',
     'glazed-fudge',
@@ -154,6 +157,7 @@ export class DonutFormComponent {
   handleSubmit(form: NgForm) {
     if (form.valid) {
       console.log(form.value);
+      this.create.emit(form.value);
     } else {
       form.form.markAllAsTouched();
     }
